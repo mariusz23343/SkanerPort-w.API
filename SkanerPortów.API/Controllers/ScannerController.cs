0677009741b95d239fea9c5b2849b234cf7bd00c
Mariusz Byczek <mariusz.byczek99@gmail.com>
2022-06-07 22:01:24 +0200

    Add project files.

---
+++ SkanerPortów.API/Controllers/ScannerController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace SkanerPortów.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ScannerController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult PostScan(Dictionary<int, bool> portScanResult)
+        {
+            return Ok();
+        }
+    }
+}
